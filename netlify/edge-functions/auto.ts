322f4be8ecc3dc9f82b8fdd6b640cd8a76a2153d
Nate Moore <nate@skypack.dev>
2022-04-20 16:25:01 -0500

    fix types

--- netlify/edge-functions/auto.ts
+++ netlify/edge-functions/auto.ts
@@ -1,6 +1,6 @@
 export default (req: Request) => {
-    const headers = {};
-    req.headers.forEach((value, key) => (headers as any)[key] = value);
+    const headers: Record<string, any> = {};
+    req.headers.forEach((value, key) => headers[key] = value);
     return new Response(JSON.stringify({ headers: headers }, null, 2));
 }
 
